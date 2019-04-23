fa383ea8bdb47274d5b51702aeebc47f86a1e1cf
edreeseg <edward.reeseg@gmail.com>
2019-04-23 16:11:56 -0400

    Setting up building blocks for auth with JWT

--- api/server.js
+++ api/server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const server = express();
 const { getUsers, getPatientsByUser, getHistory } = require('../data/helpers');
 
+const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const patientRoutes = require('./routes/patientRoutes');
+const authenticate = require('../auth/authenticate');
 
 server.use(express.json());
+server.use('/', authRoutes);
+server.use(authenticate);
 server.use('/users', userRoutes);
 server.use('/patients', patientRoutes);
 
