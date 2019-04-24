f0fa90c2251ce89523b1190dad0ed7a3e4af64d8
edreeseg <edward.reeseg@gmail.com>
2019-04-23 20:42:28 -0400

    Completed registration and login endpoints

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -6,28 +6,44 @@ const { jwtSecret } = require('../../config/secrets');
 const { getUserByName, addUser } = require('../../data/helpers');
 
 router.post('/login', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        if (!username || !password)
-            return res.status(400).json({ error: 'Request must include values for username and password.' });
-        const [user] = await getUserByName(username);
-        if (!user || !bcrypt.compareSync(password, user.password)) throw error;
-        const token = jwt.sign({ role: user.role }, jwtSecret);
-        res.json({ token });
-    } catch(error) {
-        res.status(401).json({ error: 'Invalid credentials' });
-    }
+  try {
+    const { username, password } = req.body;
+    if (!username || !password)
+      return res.status(400).json({
+        error: 'Request must include values for username and password.',
+      });
+    const [user] = await getUserByName(username);
+    if (!user || !bcrypt.compareSync(password, user.password))
+      throw new Error();
+    const token = jwt.sign({ role: user.role }, jwtSecret, { expiresIn: '1d' });
+    res.json({ token });
+  } catch (error) {
+    res.status(401).json({ error: 'Invalid credentials' });
+  }
 });
 
 router.post('/register', async (req, res) => {
+  try {
     const { username, password, email, role } = req.body;
     if (!username || !password || !email)
-        return res.status(400).json({ error: 'Request must include values for username, password, and email keys.' });
+      return res.status(400).json({
+        error:
+          'Request must include values for username, password, and email keys.',
+      });
     const credentials = { username, password, email };
     const hash = bcrypt.hashSync(credentials.password, 10);
     credentials.password = hash;
-    if (role !== undefined) credentials.role = role;
-    // Use helper function for register
+    credentials.role = role || 0;
+    const [id] = await addUser(credentials);
+    if (!id) throw new Error();
+    const token = jwt.sign({ role: role || 0 }, jwtSecret, { expiresIn: '1d' });
+    res.status(201).json({ token });
+  } catch (error) {
+    if (error.message.includes('UNIQUE constraint')) {
+      res.status(400).json({ error: 'Email or username already exist.' });
+    }
+    res.status(401).json({ error: 'Invalid credentials' });
+  }
 });
 
 module.exports = router;
--- data/helpers.js
+++ data/helpers.js
@@ -4,38 +4,45 @@ const environment = process.env.DB_ENV || 'development';
 const db = knex(config[environment]);
 
 module.exports = {
-    getUsers,
-    getPatientsByUser,
-    getPatient,
-    getHistory,
+  getUsers,
+  getUserByName,
+  addUser,
+  getPatientsByUser,
+  getPatient,
+  getHistory,
 };
 
-function getUsers(){ // User ID
-    return db('users');
+function getUsers() {
+  // User ID
+  return db('users');
 }
 
-function getUserByName(username){
-    return db('users').where({ username });
+function getUserByName(username) {
+  return db('users').where({ username });
 }
 
-function addUser(user){
-    return db('users').insert(user);
+function addUser(user) {
+  return db('users').insert(user);
 }
 
-function getPatientsByUser(id){ // User ID
-    return db('patients')
-        .where({ 'patients.userId': id });
+function getPatientsByUser(id) {
+  // User ID
+  return db('patients').where({ 'patients.userId': id });
 }
 
-function getPatient(id){ // Patient ID
-    return db('patients')
-        .where({ id });
+function getPatient(id) {
+  // Patient ID
+  return db('patients').where({ id });
 }
 
-function getHistory(id){ // Patient ID
-    return db('patients')
-        .where({ 'patients.id': id })
-        .join('patient_immunizations as PI', { 'patients.id': 'PI.patientId' })
-        .join('immunizations', { 'immunizations.id': 'PI.immunizationId' })
-        .select('immunizations.name as immunization_name', 'PI.appointmentDate as appointment_date');
-    }
+function getHistory(id) {
+  // Patient ID
+  return db('patients')
+    .where({ 'patients.id': id })
+    .join('patient_immunizations as PI', { 'patients.id': 'PI.patientId' })
+    .join('immunizations', { 'immunizations.id': 'PI.immunizationId' })
+    .select(
+      'immunizations.name as immunization_name',
+      'PI.appointmentDate as appointment_date'
+    );
+}
