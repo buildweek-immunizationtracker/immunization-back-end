8b1bb551775f7a46ac9582038d0e8bb89acf619c
edreeseg <edward.reeseg@gmail.com>
2019-04-25 22:14:19 -0400

    Correcting user endpoints' handling of ID from req.decoded

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -10,44 +10,54 @@ const {
 
 router.get('/', async (req, res) => {
   try {
-    const { id } = req.decoded;
-    const [user] = await getUser(id);
-    res.json({ user });
+    const [id] = req.decoded.id;
+    const [result] = await getUser(id);
+    const { password, ...user } = result;
+    if (!user.providerId) delete user.providerId;
+    return res.json({ user });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error });
   }
 });
 
 router.put('/', async (req, res) => {
   try {
-    const { id } = req.decoded;
+    const [id] = req.decoded.id;
     const { username, password, email } = req.body;
     const keysToUpdate = {};
     if (!username && !password && !email)
-      return res.status(400).json({ error: 'Please provide key to update on user.' });
+      return res
+        .status(400)
+        .json({ error: 'Please provide key to update on user.' });
     if (username) keysToUpdate.username = username;
     if (password) keysToUpdate.password = bcrypt.hashSync(password, 10);
     if (email) keysToUpdate.email = email;
     const numberOfUpdated = await updateUser(id, keysToUpdate);
     if (!numberOfUpdated)
-      return res.status(404).json({ error: 'No user with that ID found.  Please reauthenticate and try again.' });
+      return res.status(404).json({
+        error:
+          'No user with that ID found.  Please reauthenticate and try again.',
+      });
     const [success] = await getUser(id);
     delete success.password;
     if (!success.providerId) delete success.providerId;
     res.status(200).json({ success });
-  } catch(error) {
+  } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
 router.delete('/', async (req, res) => {
   try {
-    const { id } = req.decoded;
+    const [id] = req.decoded.id;
     const usersDeleted = await deleteUser(id);
     if (!usersDeleted)
-      return res.status(404).json({ error: 'No user by that ID found.  Please reauthenticate and try again.' });
+      return res.status(404).json({
+        error:
+          'No user by that ID found.  Please reauthenticate and try again.',
+      });
     res.json({ usersDeleted });
-  } catch(error) {
+  } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
