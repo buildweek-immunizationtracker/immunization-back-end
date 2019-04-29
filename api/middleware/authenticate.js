ab53165ef9e9cc029f013d6fdac688f3d8bb8403
edreeseg <edward.reeseg@gmail.com>
2019-04-28 23:05:51 -0400

    Changing authenticate function to async

--- api/middleware/authenticate.js
+++ api/middleware/authenticate.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { jwtSecret } = require('../../config/secrets');
 const { getUser } = require('../../data/helpers');
 
-function authenticate(req, res, next) {
+async function authenticate(req, res, next) {
   try {
     const header = req.get('Authorization');
     if (header) {
@@ -10,9 +10,11 @@ function authenticate(req, res, next) {
       const decoded = jwt.verify(token, jwtSecret); // Will throw error if not successful
       const user = await getUser(decoded.id);
       if (!user)
-        throw new Error('Token not associated with a valid user, please reauthenticate.');
+        throw new Error(
+          'Token not associated with a valid user, please reauthenticate.'
+        );
       req.decoded = decoded;
-      return next(); 
+      return next();
     }
     res
       .status(401)
