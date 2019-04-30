0369636d57aa4d00338056e2f3a3ffb96e4c1c1e
edreeseg <edward.reeseg@gmail.com>
2019-04-30 10:15:38 -0400

    Refactoring userRoutes

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -24,26 +24,28 @@ router.put('/', async (req, res) => {
   try {
     const id = req.decoded.id;
     const { username, password, email } = req.body;
-    const keysToUpdate = {};
-    if (!username && !password && !email)
-      return res
-        .status(400)
-        .json({ error: 'Please provide key to update on user.' });
-    if (username) keysToUpdate.username = username;
-    if (password) keysToUpdate.password = bcrypt.hashSync(password, 10);
-    if (email) keysToUpdate.email = email;
+    if (!username && !password && !email) throw new Error(400);
+    const keysToUpdate = { 
+      username, 
+      password: password ? bcrypt.hashSync(password, 10) : undefined, 
+      email };
+    for (let key in keysToUpdate) 
+      if (!keysToUpdate[key]) delete keysToUpdate[key];
     const numberOfUpdated = await updateUser(id, keysToUpdate);
-    if (!numberOfUpdated)
-      return res.status(404).json({
-        error:
-          'No user with that ID found.  Please reauthenticate and try again.',
-      });
+    if (!numberOfUpdated) throw new Error(404);
     const success = await getUser(id);
     delete success.password;
     if (!success.providerId) delete success.providerId;
     res.status(200).json({ success });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    switch(error.message) {
+      case 400:
+        return res.status(400).json({ error: 'Please provide key to update on user.' });
+      case 404:
+        return res.status(404).json({ error: 'No user with that ID found.  Please reauthenticate and try again.' });
+      default:
+        res.status(500).json({ error: error.message });
+    }
   }
 });
 
@@ -54,14 +56,15 @@ router.delete('/', async (req, res) => {
     let usersDeleted; // If the user is a provider, deleting the entry in the providers table will cause a cascade that will delete the user account
     if (user.providerId) usersDeleted = await deleteProvider(user.providerId);
     else usersDeleted = await deleteUser(id);
-    if (!usersDeleted)
-      return res.status(404).json({
-        error:
-          'No user by that ID found.  Please reauthenticate and try again.',
-      });
+    if (!usersDeleted) throw new Error(404);
     res.json({ usersDeleted });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    switch(error.message) {
+      case 404:
+        return res.status(404).json({ error: 'No user by that ID found.  Please reauthenticate and try again.' });
+      default:
+        res.status(500).json({ error: error.message });
+    }
   }
 });
 
