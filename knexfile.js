1855c7f6615a2a1cb72b1767b2804da48ea9e75e
edreeseg <edward.reeseg@gmail.com>
2019-05-02 09:16:31 -0400

    Initial setup for Travis CI and connection to postgres DB

--- knexfile.js
+++ knexfile.js
@@ -2,48 +2,65 @@
 require('dotenv').config();
 module.exports = {
   development: {
-    // client: 'pg',
-    // connection: `postgres://${process.env.DB_USERNAME}:${
-    //   process.env.DB_PASSWORD
-    // }@localhost/immunizations`,
-    client: 'sqlite3',
-    connection: {
-      filename: './data/dev.sqlite3',
-    },
+    // LOCAL PG:
+
+    client: 'pg',
+    connection: `postgres://${process.env.DB_USERNAME}:${
+      process.env.DB_PASSWORD
+    }@localhost/immunizations`,
+
+    // LOCAL SQLITE3:
+
+    // client: 'sqlite3',
+    // connection: {
+    //   filename: './data/dev.sqlite3',
+    // },
+    
     migrations: {
       directory: './data/migrations',
     },
     seeds: {
       directory: './data/seeds/dev',
     },
-    pool: {
-      afterCreate: (conn, cb) => {
-        conn.run('PRAGMA foreign_keys = ON', cb);
-      },
-    },
+    // pool: {
+    //   afterCreate: (conn, cb) => {
+    //     conn.run('PRAGMA foreign_keys = ON', cb);
+    //   },
+    // },
     useNullAsDefault: true,
   },
 
   testing: {
-    // client: 'pg',
+    client: 'pg',
+    // LOCAL PG:
+
     // connection: `postgres://${process.env.DB_USERNAME}:${
     //   process.env.DB_PASSWORD
     // }@localhost/immunizations_test`,
-    client: 'sqlite3',
-    connection: {
-      filename: './data/testing.sqlite3',
-    },
+
+    // LOCAL SQLITE3:
+
+    // client: 'sqlite3',
+    // connection: {
+    //   filename: './data/testing.sqlite3',
+    // },
+
+    // TRAVIS CI:
+
+    client: 'pg',
+    connection: 'postgres://postgres@127.0.0.1:5432/travis_ci_test',
     migrations: {
       directory: './data/migrations',
     },
+
     seeds: {
       directory: './data/seeds/testing',
     },
-    pool: {
-      afterCreate: (conn, cb) => {
-        conn.run('PRAGMA foreign_keys = ON', cb);
-      },
-    },
+    // pool: {
+    //   afterCreate: (conn, cb) => {
+    //     conn.run('PRAGMA foreign_keys = ON', cb);
+    //   },
+    // },
     useNullAsDefault: true,
   },
 
