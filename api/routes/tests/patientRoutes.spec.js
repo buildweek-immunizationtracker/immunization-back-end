5a4bbff6a7509fdf82337bc488e8190d791c6858
edreeseg <edward.reeseg@gmail.com>
2019-05-02 20:07:48 -0400

    Restructuring test to close connection in afterAll block

--- api/routes/tests/patientRoutes.spec.js
+++ api/routes/tests/patientRoutes.spec.js
@@ -1,35 +1,43 @@
-const server = require('../../server');
-const request = require('supertest');
+const app = require('../../server');
+const supertest = require('supertest');
 const db = require('../../../data/dbConfig');
-  
-  const testPatient = {
-    firstName: 'Test',
-    lastName: 'Patient',
-    birthDate: '1970-01-01'
-  };
-  let token;
-  beforeAll(async () => {
-    const response = await request(server)
-      .post('/login')
-      .send({ username: 'john_doe', password: 'password' })
-      .set('Content-Type', 'application/json');
-    token = `Bearer ${response.body.token}`;
-  });
-  it('Should return appropriate response',  async () => {
-    const response = await request(server)
+let request, server;
+
+const testPatient = {
+  firstName: 'Test',
+  lastName: 'Patient',
+  birthDate: '1970-01-01',
+};
+let token;
+beforeAll(done => {
+  server = app.listen(done);
+  request = supertest.agent(server);
+});
+beforeEach(async () => {
+  const response = await request
+    .post('/login')
+    .send({ username: 'john_doe', password: 'password' })
+    .set('Content-Type', 'application/json');
+  token = `Bearer ${response.body.token}`;
+});
+afterAll(done => {
+  server.close(done);
+});
+it('Should return appropriate response', async () => {
+  const response = await request
+    .post('/patients')
+    .send(testPatient)
+    .set('Authorization', token);
+  expect(response.status).toBe(201);
+  expect(typeof response.body.success.id).toBe('string');
+});
+describe('POST /patients', () => {
+  it('Should return 400 BAD REQUEST if mandatory keys are not sent', async () => {
+    const { lastName, ...incorrectPatient } = testPatient;
+    const response = await request
       .post('/patients')
-      .send(testPatient)
+      .send(incorrectPatient)
       .set('Authorization', token);
-    expect(response.status).toBe(201);
-    expect(typeof response.body.success.id).toBe('string');
+    expect(response.status).toBe(400);
   });
-  describe('POST /patients', () => {
-    it('Should return 400 BAD REQUEST if mandatory keys are not sent', async () => {
-      const { lastName, ...incorrectPatient } = testPatient;
-      const response = await request(server)
-        .post('/patients')
-        .send(incorrectPatient)
-        .set('Authorization', token);
-      expect(response.status).toBe(400);
-    });
 });
